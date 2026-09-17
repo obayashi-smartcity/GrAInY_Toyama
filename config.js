50cd6f3b62d17901f11f6b0b159fd4ace5828a0c
OC_Smartcity <hosokawa.takaaki@obayashi.co.jp>
2026-09-17 10:50:48 +0900

    config GPS permission

---
+++ config.js
@@ -0,0 +1,12 @@
+// GrAInY configuration
+window.APP_CONFIG = {
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwNK-fs6ucvjPYntcSlvMBMF1oHr5GArVlnSeqPiPD1uR3GG7B2bobIBnk9FL2LVhKAyw/exec",
+  FIELD_JSON_URL: "EventList.json",
+  SECRET_KEY_STORAGE_NAME: "fieldCollector_secretKey",
+  USER_NAME_STORAGE_NAME: "fieldCollector_userName",
+  DEFAULT_LAT: 36.6953,
+  DEFAULT_LNG: 137.2130,
+  IMAGE_MAX_SIDE: 1280,
+  IMAGE_QUALITY: 0.75,
+  ALLOW_STEP2_WITHOUT_GPS: true
+};
